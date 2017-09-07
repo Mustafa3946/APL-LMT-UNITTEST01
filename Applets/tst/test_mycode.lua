618fcc0b0293a29f1eaa79dc8eee1b00d79c9c26
VARIANT\Mohammad.Mustafa <mustafa.m3946@gmail.com>
2017-09-07 11:45:42 +1000

    [TD-26]

--- Applets/tst/test_mycode.lua
+++ Applets/tst/test_mycode.lua
@@ -1,7 +1,34 @@
 
-local luaunit = require('src.luaunit')
+local luaunit = require('tst.luaunit')
 
 TestAdd = {}
+
+function add(v1,v2)
+    -- add positive numbers
+    -- return 0 if any of the numbers are 0
+    -- error if any of the two numbers are negative
+    if v1 < 0 or v2 < 0 then
+        error('Can only add positive or null numbers, received '..v1..' and '..v2)
+    end
+    if v1 == 0 or v2 == 0 then
+        return 0
+    end
+    return v1+v2
+end
+
+function div(v1,v2)
+    -- divide positive numbers
+    -- return 0 if any of the numbers are 0
+    -- error if any of the two numbers are negative
+    if v1 < 0 or v2 < 0 then
+        error('Can only divide positive or null numbers, received '..v1..' and '..v2)
+    end
+    if v1 == 0 or v2 == 0 then
+        return 0
+    end
+    return v1/v2
+end
+
 function TestAdd:testAddPositive()
     luaunit.assertEquals(add(1,1),2)
 end
